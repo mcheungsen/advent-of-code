c0db2641c9ef04b7ca9825c5d854997b51d90d1a
mcheungsen <maevachgsn@gmail.com>
2026-04-24 14:10:57 +0200

    :sparkles: Trebuchet Part 1

---
+++ 2023/day_1/trebuchet_part1.py
@@ -0,0 +1,22 @@
+import time
+start_time = time.time()
+
+file = open("2023/day_1/input.txt")
+lines = [line.strip("\n") for line in file]
+
+sum = 0
+
+for line in lines :
+    index = 0
+    first_digit = last_digit = None
+    while ((first_digit is None or last_digit is None)
+        and index < len(line)) :
+        if first_digit is None and line[index].isnumeric() :
+            first_digit = line[index]
+        if last_digit is None and line[len(line)-1-index].isnumeric()  :
+            last_digit = line[len(line)-1-index]
+        index += 1
+    sum += int(first_digit+last_digit)
+
+print(f"the result is = {sum}")
+print("--- %s seconds ---" % (time.time() - start_time))
