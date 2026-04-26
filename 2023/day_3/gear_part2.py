dedbbb7d262576ca7016cdf2402e2e49c2ce276c
mcheungsen <maevachgsn@gmail.com>
2026-04-26 20:50:56 +0200

    :sparkles: day 3 part 2

--- 2023/day_3/gear_part2.py
+++ 2023/day_3/gear_part2.py
@@ -1,6 +1,62 @@
-# TODO Day 3 Part 2
+import time, math
+start_time = time.time()
 
-'''Doing Day 5 first. Will do this after'''
 
+result = 0
+file = open("2023/day_3/input.txt")
+lines = [line.strip("\n") for line in file]
 
-import time
+def get_indexes_neighbors(index_line:int, index_char: int):
+    # TODO : Récup les indexes qui sont des nombres autour du symbole
+    neighbors_indexes = []
+    
+    for i_line in range(-1,2):
+        for j_char in range(-1,2):
+            current_line = index_line + i_line
+            current_char = index_char + j_char
+            # in range ?
+            if (current_line >= 0 and current_line<len(lines)
+                and current_char >= 0 and current_char<len(lines[current_line])):
+                # Number ?
+                if lines[index_line+i_line][index_char+j_char].isnumeric():
+                    neighbors_indexes.append((current_line, current_char))
+    return neighbors_indexes
+
+def get_neighbors_numbers(index_line:int, index_char: int):
+    indexes_numbers = get_indexes_neighbors(index_line, index_char)
+    numbers = []
+    
+    while(len(indexes_numbers) > 0):
+        index_line = indexes_numbers[0][0]
+        index_char = indexes_numbers[0][1]
+        
+        line = lines[index_line]
+        # step 1 : first index from this index line
+        while ((index_char-1 >= 0 ) # in range 
+        and (line[index_char-1].isnumeric())) :
+            index_char -= 1
+            
+        # step 2 : get the whole number and check if in indexes list -> remove when used
+        res = ""
+        while(index_char < len(line) # in range 
+        and line[index_char].isnumeric()): 
+            res += line[index_char]
+            current_tuple = (index_line, index_char)
+            if current_tuple in indexes_numbers :
+                indexes_numbers.remove(current_tuple)
+            index_char += 1
+        numbers.append(int(res))
+    return numbers
+
+for index_line in range(len(lines)) :
+    line = lines[index_line]
+    for index_char in range(len(lines[index_line])) :
+        current_char = line[index_char]
+        if current_char == "*":
+            neighbors_numbers = get_neighbors_numbers(index_line, index_char)
+            if len(neighbors_numbers) == 2:
+                result += math.prod(neighbors_numbers)
+            
+
+print(f"The result is = {result}")
+print("--- %s seconds ---" % (time.time() - start_time))
