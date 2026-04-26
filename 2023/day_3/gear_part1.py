8f0062e2210d18205cb1a6e91e2682722d5bf886
mcheungsen <maevachgsn@gmail.com>
2026-04-26 20:29:54 +0200

    :sparkles: day 3 part 1

--- 2023/day_3/gear_part1.py
+++ 2023/day_3/gear_part1.py
@@ -1,6 +1,3 @@
-'''Doing Day 5 first. Will do this after'''
-
-
 import time
 start_time = time.time()
 
@@ -8,18 +5,52 @@ start_time = time.time()
 result = 0
 file = open("2023/day_3/input_example.txt")
 lines = [line for line in file]
+
 def get_indexes_neighbors(index_line:int, index_char: int):
     # TODO : Récup les indexes qui sont des nombres autour du symbole
-    pass
+    neighbors_indexes = []
+    
+    for i_line in range(-1,2):
+        for j_char in range(-1,2):
+            current_line = index_line + i_line
+            current_char = index_char + j_char
+            # in range ?
+            if (current_line >= 0 and current_line<len(lines)
+                and current_char >= 0 and current_char<len(lines[current_line])):
+                # Number ?
+                if lines[index_line+i_line][index_char+j_char].isnumeric():
+                    neighbors_indexes.append((current_line, current_char))
+    return neighbors_indexes
 
 def get_neighbors_numbers(index_line:int, index_char: int):
-    numbers = get_indexes_neighbors(index_line, index_char)
-    # TODO : parcourir les indexes et identifier les nombres
+    indexes_numbers = get_indexes_neighbors(index_line, index_char)
+    numbers = []
+    
+    while(len(indexes_numbers) > 0):
+        index_line = indexes_numbers[0][0]
+        index_char = indexes_numbers[0][1]
+        
+        line = lines[index_line]
+        # step 1 : first index from this index line
+        while ((index_char-1 >= 0 ) # in range 
+        and (line[index_char-1].isnumeric())) :
+            index_char -= 1
+            
+        # step 2 : get the whole number and check if in indexes list -> remove when used
+        res = ""
+        while(index_char < len(line) # in range 
+        and line[index_char].isnumeric()): 
+            res += line[index_char]
+            current_tuple = (index_line, index_char)
+            if current_tuple in indexes_numbers :
+                indexes_numbers.remove(current_tuple)
+            index_char += 1
+        numbers.append(int(res))
     return numbers
 
 for index_line in range(len(lines)) :
     line = lines[index_line]
-    for index_char in range(lines[index_line]) :
+    for index_char in range(len(lines[index_line])) :
         current_char = line[index_char]
         if not current_char.isnumeric() and current_char != ".":
             result += sum(get_neighbors_numbers(index_line, index_char))
--- 2023/day_3/readme.md
+++ 2023/day_3/readme.md
@@ -6,3 +6,21 @@
 2. si symbole => Récupérer les indexes des nombres voisins
 3. Pour chaque index, identifier le nombre complet. Supprimer des indexes pour éviter d'avoir des nombres répétés
 4. Additionner le résultat
+
+### Récupération des indexes
+
+Au lieu de direct récupérer les nombres, on récupère les indexes voisins des symboles. On peut visualiser ce parcourt de voisins, d'une matrice de -1 à 1. Par rapport à l'index symbole, il faut regarder de -1 à +
+```
+| 7 | . | . |
+| . | * | . |
+| 3 | 5 | . |
+```
+
+on voit ici que le `7` est à (-1,-1) par rapport à `*`.
+ainsi `3 => (1,-1)` et `5 => (1,0)` si on considère que `*` est à (0,0).
+
+### Identifier le nombre complet
+
+Pour identifier le nombre complet, à partir du premier tuple de mes indexes, je recule de 1 jusqu'à ce que ce ne soit plus un nombre. On obtient l'index de départ. Avec cet index, on incrémente de 1, et on ajoute le nombre, jusqu'à ne plus avoir de nombre.
+
+Lorsque je rencontre un tuple déjà présent dans mes indexes, je le supprime. Je répète cette étape, jusqu'à ce que mon tableau des indexes soit vide.
